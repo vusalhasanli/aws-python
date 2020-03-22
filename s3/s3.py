a555477a8fe6caeae53ce23ec90cd0da1662ff8c
Vusal <vusal@MacBook-Pro.local>
2020-03-22 04:04:50 -0700

    Add filter s3 buckets giving option from terminal, @click()...

--- s3/s3.py
+++ s3/s3.py
@@ -1,11 +1,30 @@
 import boto3
+import click
+
+
+
+session = boto3.Session(profile_name='architect')
+s3 = boto3.resource('s3')
+
+@click.command()
+@click.option( '--list', default=None, help='list s3 buckets')
+
+def list_buckets(list):
+    if list:
+        print('---- filtered ----')
+        buckets = s3.buckets.filter(Prefix='my-')
+    else:
+        buckets = s3.buckets.all()
+        # print("unfiltered")
+    
+    for bucket in buckets:
+        print(bucket.name)
+
+
 
 
 if __name__ == "__main__":
-    user_name = input("User name: ")
-    session = boto3.Session(profile_name=user_name)
-    s3 = boto3.resource('s3')
+    list_buckets()
+    
 
-
-    for bucket in s3.buckets.all():
-        print(bucket.name)
+    
