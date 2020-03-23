c7cd24388ff0138cb6d48d6fa790b73987255c49
Vusal <vusal@MacBook-Pro.local>
2020-03-22 18:47:04 -0700

    Add list-buckets subcommand

--- Readme.md
+++ Readme.md
@@ -9,8 +9,9 @@ Use boto3 to make python scripts for each AWS services
 
 
 #Configuration 
-   - Set up profile -->
-        `aws configure --profile default`
+    - Set up profile -->
+        `aws configure --profile {user}`
 
 
-#
+#To list buckets run `python3 s3.py list-buckets`
+#For help run `python3 s3.py --help`
--- s3/s3.py
+++ s3/s3.py
@@ -2,29 +2,47 @@ import boto3
 import click
 
 
-
 session = boto3.Session(profile_name='architect')
 s3 = boto3.resource('s3')
+buckets = s3.buckets.all()
 
-@click.command()
-@click.option( '--list', default=None, help='list s3 buckets')
+@click.group()
+def buckets():
+    """Commands for buckets"""
 
-def list_buckets(list):
-    if list:
-        print('---- filtered ----')
-        buckets = s3.buckets.filter(Prefix='my-')
-    else:
-        buckets = s3.buckets.all()
-        # print("unfiltered")
-    
+@buckets.command()
+@click.option( '--name', default=None, help='list s3 buckets')
+def list_buckets(name):
+    buckets = s3.buckets.all()
+
     for bucket in buckets:
         print(bucket.name)
+
+
+# @buckets.command()
+# @click.option('--create', default=None, help='create a bucket')
+# def create_bucket():
+#     
+
+
+
+
+
+
+# ---------->> Continue later
+# def list_objects(bucket, buckets):
+#     if bucet:
+#         bucket.name = bucet
+#         for obj in bucket.objects.filter(Prefix='photos/'):
+#                 print('{0}:{1}'.format(bucke.name, obj.key))
+#         # print(bucket.name)
+#     else: print("something went wrong")
 
 
 
 
 if __name__ == "__main__":
-    list_buckets()
+    buckets()
     
 
     
